0d46f4246d33a2f1c7cecb6c58e0f0d71125b686
ScriptCamilo <script.camilo@gmail.com>
2022-10-10 19:47:31 -0300

    feat: fifth module finished multi databases

---
+++ 07-multi-db/src/db/strategies/postgres.js
@@ -0,0 +1,14 @@
+const ICrud = require('./interfaces/interfaceCrud');
+
+
+class Postgres extends ICrud {
+  constructor() {
+    super();
+  }
+
+  create(item) {
+    console.log('Item saved in Postgres');
+  }
+}
+
+module.exports = Postgres;
